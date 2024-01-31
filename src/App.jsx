d6f445122854478b8c53ebcdbab6fc0651ce06f2
Harish Karthick S <harish.s@kalvium.community>
2024-01-31 11:47:49 +0530

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import PureCounterComponent from './Component/PureCounterComponent'
+import PureCounterComponent from './Component/PureCounterCOmponent'
 
 import SimpleCounterComponent from './Component/SimpleCounterComponent'
 import './App.css'
